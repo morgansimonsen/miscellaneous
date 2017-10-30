f9bc9e70f1b21ac0de2353de7bff3c21a929599d
morgansimonsen <morgan@simonsen.bz>
2017-10-30 12:24:17 +0100

    Added script for Giniko BBC World Service

---
+++ GinikoBBCWorld.ps1
@@ -0,0 +1,21 @@
+<#
+	GinikoBBCWorld.ps1
+
+	Script to use VLC to play the BBC World Service stream using  VLC
+#>
+
+$GinikoURL = "giniko.com/watch.php?id=216"
+$VLCPath = "C:\Program Files\VideoLAN\VLC\vlc.exe"
+$userAgent = ":http-user-agent=Mozilla/5.0"
+
+# Get the web page with the auth key
+$HTML = Invoke-WebRequest -Uri $GinikoURL
+
+# Extract the URL and authN key
+$video = $HTML.ParsedHtml.getElementsByTagName("source")
+$video2 = $video.ie8_item().outerhtml -split " "
+#$video2 = $video2[1] -split "\?" -replace '\"'
+$url = $video2[1] -replace "src=",""
+
+# Start the stream
+Start-Process -FilePath $VLCPath -ArgumentList ($url+" "+$userAgent)
