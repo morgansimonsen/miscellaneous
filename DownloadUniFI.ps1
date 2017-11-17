90ee0ebd9d1baa499f740c3359560b65a79d976b
Morgan Simonsen <morgan@simonsen.bz>
2017-11-17 13:22:10 +0100

    Added UniFi downloader script

---
+++ DownloadUniFI.ps1
@@ -0,0 +1,4 @@
+﻿# Download UniFI SW from URI
+
+$UniFiDownloadURI = [System.Uri]$args[0]
+Start-BitsTransfer -Source $UniFiDownloadURI.AbsoluteUri -Destination (($UniFiDownloadURI.Segments[-1] -replace ".exe","")+"-"+($UniFiDownloadURI.Segments[-2] -replace "/",".exe"))
