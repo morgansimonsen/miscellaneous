0ccc737c8e9f7ec9812b64adcd0803247be17a51
morgansimonsen <morgan@simonsen.bz>
2018-04-20 15:45:07 +0200

    improved unifi installer

--- DownloadUniFI.ps1
+++ DownloadUniFI.ps1
@@ -1,4 +1,21 @@
-﻿# Download UniFI SW from URI
+﻿# Upgrade Unifi Controller
+# Morgan Simonsen
 
+# Download UniFI SW from URI
 $UniFiDownloadURI = [System.Uri]$args[0]
-Start-BitsTransfer -Source $UniFiDownloadURI.AbsoluteUri -Destination (($UniFiDownloadURI.Segments[-1] -replace ".exe","")+"-"+($UniFiDownloadURI.Segments[-2] -replace "/",".exe"))
+$UniFiInstaller = (($UniFiDownloadURI.Segments[-1] -replace ".exe","")+"-"+($UniFiDownloadURI.Segments[-2] -replace "/",".exe"))
+Start-BitsTransfer -Source $UniFiDownloadURI.AbsoluteUri -Destination $UniFiInstaller
+
+# Stop current Unifi process
+$UniFiParentProcess = Get-CimInstance Win32_Process | Where-Object { $_.CommandLine -like "*javaw*UniFi*ace.jar*" }
+add-type -AssemblyName microsoft.VisualBasic
+add-type -AssemblyName System.Windows.Forms
+$MainWindowTitle = (get-process -Id  $UniFiParentProcess.ProcessId | Select-Object *).MainWindowTitle
+( Get-Process -Id $UniFiParentProcess.ProcessId ).CloseMainWindow()
+[Microsoft.VisualBasic.Interaction]::AppActivate($MainWindowTitle)
+[System.Windows.Forms.SendKeys]::SendWait("~")
+#taskkill.exe /T /PID $UniFiParentProcess.ProcessId /F
+
+# Install new Unifi
+Start-Process -FilePath $UniFiInstaller -ArgumentList "/S" -Wait
+# add keystrokes here too to confirm the prompts...
