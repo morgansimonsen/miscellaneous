7752c39679845d1119457f9bb7aee917039be685
Morgan Simonsen <morgan@simonsen.bz>
2018-05-31 16:14:56 +0200

    Fixed broken parsing in PS

--- GinikoBBCWorld.ps1
+++ GinikoBBCWorld.ps1
@@ -5,6 +5,7 @@
 
 	References:
 	- https://wiki.videolan.org/VLC_command-line_help/
+	- http://nimlive1.giniko.com/bbcworldnews/bbcworld/playlist.m3u8?wmsAuthSign=c2VydmVyX3RpbWU9NS8zMS8yMDE4IDE6MTc6MzcgUE0maGFzaF92YWx1ZT1kM1dWaTNmMVhpa1V3WDlKK1A4Zm5nPT0mdmFsaWRtaW51dGVzPTE0NDA=
 #>
 
 $GinikoURL = "http://www.giniko.com/watch.php?id=216"
@@ -12,13 +13,15 @@ $VLCPath = Get-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersi
 $userAgent = ":http-user-agent=Mozilla/5.0"
 
 # Get the web page with the auth key
-$HTML = Invoke-WebRequest -Uri $GinikoURL
+$HTML = Invoke-WebRequest -Uri $GinikoURL #-UseBasicParsing
 
 # Extract the URL and authN key
-$video = $HTML.ParsedHtml.getElementsByTagName("source")
-$video2 = $video.ie8_item().outerhtml -split " "
+$urldata = $html.Content -split "[`r`n]" | select-string -pattern "wmsAuthSign"
+$url = ($urldata -split '"')[5]
+#$video = $HTML.ParsedHtml.getElementsByTagName("source")
+#$video2 = $video.ie8_item().outerhtml -split " "
 #$video2 = $video2[1] -split "\?" -replace '\"'
-$url = $video2[1] -replace "src=",""
+#$url = $video2[1] -replace "src=",""
 
 # Start the stream
 Start-Process -FilePath $VLCPath.'(default)' -ArgumentList ($url+" "+$userAgent+" --video-on-top --qt-minimal-view --qt-opacity=1.0")
